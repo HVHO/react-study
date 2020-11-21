287d76647e7b0cc8347bc0b363e57a30ffe8f507
HVHO <hvho1119@gmail.com>
2020-11-21 17:25:16 +0900

    Implement kakao map api

--- holiday-pharmacy/src/App.js
+++ holiday-pharmacy/src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import PhoneForm from './components/PhoneForm';
-import PhoneInfoList from './components/PhoneInfoList';
+import MapContainer from './components/KakaoMapContainer';
 
 class App extends Component {
 
-  id = 0;
-
-  state = {
-    information : [
-    ]
-  }
-
-  handleCreate = (data) => {
-    console.log({data});
-    const { information } = this.state;
-    this.setState(
-      {information: information.concat({id: this.id++, ...data})}
-    ) 
-  }
-
   render() {
+   
+
     return (
       <div>
-        <PhoneForm
-          onCreate={this.handleCreate}
-        />
-        <PhoneInfoList data={this.state.information}/>
-        <div>{JSON.stringify(this.state)}</div>
+        <MapContainer></MapContainer>
       </div>
     );
   }
